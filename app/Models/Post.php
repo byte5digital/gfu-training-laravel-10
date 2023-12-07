7ee7d6f561b1cbde9b8dcd59223c3e59d6a9db12
Timo Paul <tpaul@byte5.de>
2023-12-07 12:54:46 +0100

    Created `Model` for `Posts`. `Factory` and `Seeder` for `Posts` and `Users`

---
+++ app/Models/Post.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\App;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Post extends Model
+{
+    use HasFactory;
+    use HasSlug;
+    use SoftDeletes;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'text',
+    ];
+
+    /**
+     * @param array $options
+     * @return bool
+     */
+    public function save(array $options = array()): bool
+    {
+        // assign to current user
+        if (empty($this->user_id)) {
+            $this->user_id = auth()->id();
+        }
+
+        return parent::save($options);
+    }
+
+    /**
+     * returns the relation to the user has posted this
+     *
+     * @return HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, App::make(User::class)->getKeyName(), 'user_id');
+    }
+
+    /**
+     * @return SlugOptions
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->limit(1)->first()->getKey(),
+            'title' => fake()->text(20),
+            'text'  => fake()->paragraphs(rand(3, 10), true),
+        ];
+    }
+}
